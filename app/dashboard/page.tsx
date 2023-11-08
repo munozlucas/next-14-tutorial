06a61f65b588d6ce19f6b602078905706cbe1c8f
munozlucas <lgaston.munoz@gmail.com>
2023-11-07 21:38:57 -0300

    remove test fetch

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,4 @@
-import Test from "./Test";
-
-async function getUsers() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const json = await res.json();
-  return json;
-}
-
 export default async function Page() {
-  const users = await getUsers();
   return (
     <div
       style={{
@@ -15,10 +6,7 @@ export default async function Page() {
         flexDirection: "column",
       }}
     >
-      {users.map((user) => (
-        <span key={user.id}>{user.name}</span>
-      ))}
-      <Test />
+      dashboard page
     </div>
   );
 }
